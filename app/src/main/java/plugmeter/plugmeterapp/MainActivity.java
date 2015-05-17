16eb14c9ac082727c0cbc037ae9b018372ad93e1
Leonardo Marques <leommarques7@gmail.com>
2015-05-17 13:42:21 -0300

    .

--- app/src/main/java/plugmeter/plugmeterapp/MainActivity.java
+++ app/src/main/java/plugmeter/plugmeterapp/MainActivity.java
@@ -1,38 +1,57 @@
 package plugmeter.plugmeterapp;
 
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.app.Activity;
+import android.widget.TextView;
 
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
 
-public class MainActivity extends ActionBarActivity {
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.ViewById;
+import org.apache.http.Header;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+import java.io.UnsupportedEncodingException;
+
+@EActivity(R.layout.activity_main)
+public class MainActivity extends Activity {
+
+    @ViewById
+    TextView textView;
+
+    @AfterViews
+    public void afterViews() {
+        new AsyncHttpClient().get("192.168.4.1", new AsyncHttpResponseHandler() {
+
+            @Override
+            public void onStart() {
+                textView.setText("onStart");
+            }
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
+                try {
+                    textView.setText(new String(response, "UTF-8"));
+                } catch (UnsupportedEncodingException e) {
+                    textView.setText("UnsupportedEncodingException");
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
+                textView.setText("Erro - Conecte na rede Plug Meter");
+            }
+
+            @Override
+            public void onRetry(int retryNo) {
+                textView.setText("onRetry");
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    @Click
+    public void textView() {
+        afterViews();
     }
 }
